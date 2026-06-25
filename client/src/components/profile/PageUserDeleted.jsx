21c80a5f646f02a5f7ddc94f05d5cf58d69c1946
Fuhar Sagar <fuharsagar28dec@gmail.com>
2026-06-25 17:58:34 +0530

    updated text

--- client/src/components/profile/PageUserDeleted.jsx
+++ client/src/components/profile/PageUserDeleted.jsx
@@ -27,7 +27,7 @@ export default function PageUserDeleted() {
           </Typography>
 
           <Typography sx={{ color: 'text.secondary' }}>
-            Thank you for using SplitApp! <br/>
+            Thank you for using SettleUp! <br/>
           </Typography>
           </Box>
           <Box
